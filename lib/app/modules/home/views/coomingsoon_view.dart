1a2d341425e448179ccb6069ee98edd9560c4efb
hrswcksono <hariswicaksono250999@gmail.com>
2022-03-25 10:16:22 +0700

    tabbar

---
+++ lib/app/modules/home/views/coomingsoon_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+class CoomingsoonView extends GetView {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'CoomingsoonView is working',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:movie_app_flutter/app/modules/home/views/tabbarPage.dart';
+import 'package:tab_indicator_styler/tab_indicator_styler.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -8,29 +10,26 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        title: Text(
-          'Star Movie',
-          style: TextStyle(color: Colors.black, fontFamily: 'SFPro'),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Text(
+            'Star Movie',
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'SFPro',
+              fontSize: 28,
+            ),
+          ),
+          centerTitle: false,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.search),
+              color: Colors.black,
+              iconSize: 35,
+            )
+          ],
         ),
-        centerTitle: false,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.search),
-            color: Colors.black,
-            iconSize: 35,
-          )
-        ],
-      ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style:
-              TextStyle(color: Colors.black, fontSize: 30, fontFamily: 'SFPro'),
-        ),
-      ),
-    );
+        body: TabBarPage());
   }
 }
